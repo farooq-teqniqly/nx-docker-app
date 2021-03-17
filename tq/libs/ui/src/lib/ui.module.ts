383dbf22cdbbc0fa7db30656e13e2d030a60fff0
Farooq <farooq@teqniqly.com>
2021-03-17 12:37:33 -0700

    first

--- tq/libs/ui/src/lib/ui.module.ts
+++ tq/libs/ui/src/lib/ui.module.ts
@@ -3,17 +3,26 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   imports: [
     CommonModule,
     MatIconModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    LayoutModule
   ],
   exports: [
-    MatIconModule,
-    MatButtonModule
-  ]
+    DashboardComponent
+  ],
+  declarations: [DashboardComponent]
 })
 export class UiModule {}
