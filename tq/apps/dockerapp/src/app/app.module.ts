51fca7f6df4b91e566b6f0659a6f9784b139989a
Farooq <farooq@teqniqly.com>
2021-03-17 22:43:10 -0700

    wip

--- tq/apps/dockerapp/src/app/app.module.ts
+++ tq/apps/dockerapp/src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { UiModule } from '@tq/ui';
+import { DashboardComponent } from 'libs/ui/src/lib/dashboard/dashboard.component';
+import { NavComponent } from 'libs/ui/src/lib/nav/nav.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent, pathMatch: 'full'
+  },
+  {
+    path: 'nav',
+    component: NavComponent, pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: DashboardComponent, pathMatch: 'full'
+  },
+];
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiModule],
+  imports: [
+    BrowserModule, 
+    RouterModule.forRoot(routes), 
+    UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
